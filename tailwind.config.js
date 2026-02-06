d35a63de08db3aef6e460d98135b9734b02ff6a0
dustyring38-star <dustyring38@gmail.com>
2026-02-06 05:42:29 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bg: "#0b0f1a",
+        card: "#11162a",
+        primary: "#6d5cff"
+      }
+    }
+  },
+  plugins: []
+};
